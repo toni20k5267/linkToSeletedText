b4ea0047008d219a934e0aa688b3c0a075a13a37
toni20k <133744466+toni20k5267@users.noreply.github.com>
2025-09-16 19:10:58 +0200

    Update background.js
    changed the way of encoding chars

--- background.js
+++ background.js
@@ -9,7 +9,7 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (!info.selectionText || !tab?.url || !tab?.id) return;
 
-  const encoded = encodeURIComponent(info.selectionText.trim());
+  const encoded = encodeTextFragment(info.selectionText.trim());
   const link = `${tab.url.split('#')[0]}#:~:text=${encoded}`;
 
   chrome.scripting.executeScript({
@@ -25,3 +25,34 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     args: [link]
   });
 });
+
+// shoulda used it in the beggining LOL cuz man previous method was dogshit
+function encodeTextFragment(text) {
+  return text
+    .replace(/%/g, '%25')
+    .replace(/-/g, '%2D')
+    .replace(/\?/g, '%3F')
+    .replace(/#/g, '%23')
+    .replace(/&/g, '%26')
+    .replace(/=/g, '%3D')
+    .replace(/"/g, '%22')
+    .replace(/\'/g, '%27')
+    .replace(/</g, '%3C')
+    .replace(/>/g, '%3E')
+    .replace(/\[/g, '%5B')
+    .replace(/]/g, '%5D')
+    .replace(/{/g, '%7B')
+    .replace(/}/g, '%7D')
+    .replace(/\|/g, '%7C')
+    .replace(/\\/g, '%5C')
+    .replace(/\^/g, '%5E')
+    .replace(/`/g, '%60')
+    .replace(/\n/g, '%0A')
+    .replace(/\r/g, '%0D')
+    .replace(/\t/g, '%09')
+    .replace(/\s/g, '%20');
+}
+
+
+
+// yes i used ai to give me the characters themselves. judge me, judge me idc i aint gon be placing them all myself im too lazy lol
